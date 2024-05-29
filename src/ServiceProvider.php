b455b80fad485076791319dd9f73e954bbfbfc9c
HassanDomeDenea <hassan.domedenea@gmail.com>
2024-05-29 15:01:21 +0300

    Adding Service Provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,15 @@
 
 namespace HassanDomeDenea\PrimevueServerTools;
 
-class ServiceProvider
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    public function register()
+    {
 
+    }
+
+    public function boot()
+    {
+
+    }
 }
